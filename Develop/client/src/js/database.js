b238fde605e6a9a96e2976fb61fd9762d59d1213
Negatseg <tseganehnegash@gmail.com>
2024-01-17 16:31:06 -0600

    update the database script

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,30 +15,30 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 //export const putDb = async (content) => console.error('putDb not implemented');
 export const putDb = async (content) => {
-  const db = await initdb();
+  const db = await openDB();
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
   await store.add(content);
-  await tx.done;
+  //await tx.done;
   console.log('Content added to the database:', content);
 };
 
 // TODO: Add logic for a method that gets all the content from the database
 //export const getDb = async () => console.error('getDb not implemented');
 export const getDb = async () => {
-  const db = await initdb();
+  const db = await openDB();
   const tx = db.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const allContent = await store.getAll();
-  await tx.done;
-  console.log('All content from the database:', allContent);
+  const allContent = await store.get(1);
+  //await tx.done;
+  console.log('All content from the database:', allContent.value);
   return allContent;
 };
 
 initdb();
 
-// Example of using putDb and getDb
-const exampleContent = { data: 'Some example data' };
+// // Example of using putDb and getDb
+// const exampleContent = { data: 'Some example data' };
 
-putDb(exampleContent); // Add content to the database
-getDb(); // Retrieve all content from the database
+// putDb(exampleContent); // Add content to the database
+// getDb(); // Retrieve all content from the database
